b7e26abe90c89d996f35da89ec070b10c267d8eb
Cuong Tran <clonebmn2itt@gmail.com>
2020-07-09 14:56:43 +0700

    feat: Connect mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { MessengerClient } = require('messaging-api-messenger');
+const mongoose = require('mongoose');
 const { dump } = require('dumper.js');
 const express = require('express');
 const fs = require('fs');
@@ -15,6 +16,21 @@ app.set('port', process.env.PORT || 3000);
 app.set('page_token', process.env.PAGE_TOKEN || '');
 app.set('verify_token', process.env.VERIFY_TOKEN || '');
 app.set('page_id', process.env.PAGE_ID || 4);
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
+
+mongoose.connection
+    .once('open', function () {
+        console.log("Connection database success!")
+    })
+    .on('error', function (error) {
+        console.log(error.stack);
+        process.exit(1);
+    });
 const client = new MessengerClient({
     accessToken: app.get('page_token'),
     appId: process.env.APP_ID,
@@ -75,19 +91,70 @@ function processorHook(entry) {
 
                 const menu = JSON.parse(fs.readFileSync('./menu.json'))
                 client.setMessengerProfile({
-                    get_started: {
-                        payload: 'GET_STARTED',
-                    },
                     persistent_menu: menu,
                     psid: sender
                 }).then(console.log);
                 // client.setPersistentMenu(menu).then(console.log);
             }
+            if (text == 'a') {
+                client.sendReceiptTemplate(sender, {
+                    recipient_name: 'Trần Đức Cường',
+                    order_number: '1423',
+                    currency: 'VND',
+                    payment_method: 'Tiền mặt',
+                    order_url: 'http://petersapparel.parseapp.com/order?order_id=123456',
+                    timestamp: parseInt(new Date().getTime() / 1000),
+                    elements: [
+                        {
+                            title: 'Áo phông trắng',
+                            subtitle: '100% cotton mềm',
+                            quantity: 2,
+                            price: 50000,
+                            currency: 'VND',
+                            image_url: 'https://i2.wp.com/lhomme.vn/wp-content/uploads/2019/02/WHITE-SHIRT-080.jpg?fit=678%2C1059&ssl=1',
+                        },
+                        {
+                            title: 'Áo phông nâu',
+                            subtitle: '100% cotton mềm',
+                            quantity: 1,
+                            price: 25000,
+                            currency: 'VND',
+                            image_url: 'https://cf.shopee.ph/file/e34dfc3e41caa776c3c0fb059cfb1958',
+                        },
+                    ],
+                    address: {
+                        street_1: '141 Chiến Thắng',
+                        street_2: 'Xóm 6 Nghi Văn',
+                        city: 'Hà Nội',
+                        postal_code: '94025',
+                        state: 'HN',
+                        country: 'VN',
+                    },
+                    summary: {
+                        subtotal: 75000,
+                        shipping_cost: 4900,
+                        total_tax: 6190,
+                        total_cost: 56090,
+                    },
+                    adjustments: [
+                        {
+                            name: 'Giảm giá cho người mới',
+                            amount: 20000,
+                        },
+                        {
+                            name: 'Phiếu giảm giá 10VND',
+                            amount: 10000,
+                        },
+                    ],
+                });
+
+
+            }
             client.sendText(sender, `Echo: ${text.substring(0, 200)}`);
         }
         if (event.postback) {
             switch (event.postback.payload) {
-                case "GET_STARTED2":
+                case "GET_STARTED":
 
                     break;
 

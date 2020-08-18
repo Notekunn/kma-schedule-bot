cecb1839ac952c290935de670fe33fc6c791e592
Cuong Tran <clonebmn2itt@gmail.com>
2020-08-18 13:21:26 +0700

    feat: menu export

--- handlers/assets/menu.js
+++ handlers/assets/menu.js
@@ -1,3 +1,63 @@
+const nestedOneOne = [
+  {
+    title: "Youtube 1",
+    type: "web_url",
+    url: "https://www.youtube.com/watch?v=v",
+    webview_height_ratio: "tall"
+  },
+  {
+    title: "Facebook 1",
+    type: "web_url",
+    url: "https://www.facebook.com/w",
+    webview_height_ratio: "tall"
+  },
+  {
+    title: "Trang chủ 1",
+    type: "web_url",
+    url: "https://protocol-facebook.herokuapp.com/",
+    webview_height_ratio: "tall"
+  }
+];
+const nestedOne = [
+  {
+    title: "Khám phá",
+    type: "nested",
+    call_to_actions: nestedOneOne
+  },
+  {
+    title: "Công cụ khác",
+    type: "web_url",
+    url: "https://www.facebook.com/w",
+    webview_height_ratio: "tall"
+  },
+  {
+    title: "Trang chủ",
+    type: "web_url",
+    url: "https://protocol-facebook.herokuapp.com/",
+    webview_height_ratio: "tall"
+  }
+];
+const nestedTwo = [
+  {
+    title: "Youtube",
+    type: "web_url",
+    url: "https://www.youtube.com/watch?v=v",
+    webview_height_ratio: "tall"
+  },
+  {
+    title: "Facebook",
+    type: "web_url",
+    url: "https://www.facebook.com/w",
+    webview_height_ratio: "tall"
+  },
+  {
+    title: "Trang chủ",
+    type: "web_url",
+    url: "https://protocol-facebook.herokuapp.com/",
+    webview_height_ratio: "tall",
+    messenger_extensions: true
+  }
+];
 module.exports = [
   {
     locale: "default",
@@ -8,71 +68,14 @@ module.exports = [
         payload: "GET_STARTED"
       },
       {
-        title: "Trang web",
+        title: "Công cụ",
         type: "nested",
-        call_to_actions: [
-          {
-            title: "Khám phá",
-            type: "nested",
-            call_to_actions: [
-              {
-                title: "Youtube 1",
-                type: "web_url",
-                url: "https://www.youtube.com/watch?v=v",
-                webview_height_ratio: "tall"
-              },
-              {
-                title: "Facebook 1",
-                type: "web_url",
-                url: "https://www.facebook.com/w",
-                webview_height_ratio: "tall"
-              },
-              {
-                title: "Trang chủ 1",
-                type: "web_url",
-                url: "https://protocol-facebook.herokuapp.com/",
-                webview_height_ratio: "tall"
-              }
-            ]
-          },
-          {
-            title: "Trang khác",
-            type: "web_url",
-            url: "https://www.facebook.com/w",
-            webview_height_ratio: "tall"
-          },
-          {
-            title: "Trang chủ",
-            type: "web_url",
-            url: "https://protocol-facebook.herokuapp.com/",
-            webview_height_ratio: "tall"
-          }
-        ]
+        call_to_actions: nestedOne
       },
       {
-        title: "Trang web",
+        title: "Giải trí",
         type: "nested",
-        call_to_actions: [
-          {
-            title: "Youtube",
-            type: "web_url",
-            url: "https://www.youtube.com/watch?v=v",
-            webview_height_ratio: "tall"
-          },
-          {
-            title: "Facebook",
-            type: "web_url",
-            url: "https://www.facebook.com/w",
-            webview_height_ratio: "tall"
-          },
-          {
-            title: "Trang chủ",
-            type: "web_url",
-            url: "https://protocol-facebook.herokuapp.com/",
-            webview_height_ratio: "tall",
-            messenger_extensions: true
-          }
-        ]
+        call_to_actions: nestedTwo
       }
     ]
   }

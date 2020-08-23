c3c92707aaed7f6840ffebaa411a90c52caee02c
Cuong Tran <clonebmn2itt@gmail.com>
2020-08-23 22:35:37 +0700

    style: split code prompt end

--- handlers/prompt/index.js
+++ handlers/prompt/index.js
@@ -11,27 +11,7 @@ const promptAsk = async function (ps_id, prompt_id) {
     console.log(await User.findOneAndUpdate({ ps_id }, { currentQuestion: prompt_id }));
     return;
 }
-const promptAnswer = async function (names = []) {
-    console.log('<prompt_answer>');
-    const prompts = await Prompt.find({ name: { $in: names } }).sort({ updatedAt: -1 });
-    const result = {};
-    prompts.forEach(e => {
-        if (e.isDone) result[e.name] = e.answer;
-    })
-    return result;
-}
-const promptEnd = async function (last_questions, ps_id) {
-    console.log('<prompt_end>');
-    const { client } = this;
-    switch (last_questions) {
-        case "student_pass":
-            const { student_code, student_pass } = await promptAnswer(['student_code', 'student_pass']);
-            client.sendText(ps_id, `Login with ${student_code} | ${student_pass}`);
-            break;
-        default:
-            break;
-    }
-}
+const promptEnd = require('./end');
 const promptCreate = async function (ps_id, questions) {
     console.log('<prompt_create>');
     if (!Array.isArray(questions)) questions = [questions];

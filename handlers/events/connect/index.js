410fdbb43de3087d93e4e407e2df04eadd8755dc
Cuong Tran <clonebmn2itt@gmail.com>
2020-08-24 11:16:49 +0700

    chore: handler error

--- handlers/events/connect/index.js
+++ handlers/events/connect/index.js
@@ -2,29 +2,34 @@ const utils = require('./utils');
 const Account = require('../../../models/Account');
 module.exports = async function (client, psid, { student_code, student_pass }) {
     client.sendText(psid, `Login with ${student_code} | ${student_pass}`);
-    // const data = JSON.stringify({ studentCode: student_code, password: student_pass });
-    const { data } = await utils.api.post('/users/login', { studentCode: student_code, password: student_pass });
-    if (!data.success) {
-        client.sendButtonTemplate(psid, 'Kết nối tài khoản thất bại!', [
-            {
-                type: 'web_url',
-                url: `${process.env.HOST_URL}/`,
-                title: 'Đăng nhập qua trang web',
-                webview_height_ratio: "tall",
-                messenger_extensions: true
-            },
-            {
-                type: 'postback',
-                title: 'Kết nối lại',
-                payload: 'STUDENT_CONNECT',
-            },
-        ]);
-        return;
+    try {
+        const { data } = await utils.api.post('/users/login', { studentCode: student_code, password: student_pass });
+        if (!data.success) {
+            client.sendButtonTemplate(psid, 'Kết nối tài khoản thất bại!', [
+                {
+                    type: 'web_url',
+                    url: `${process.env.HOST_URL}/`,
+                    title: 'Đăng nhập qua trang web',
+                    webview_height_ratio: "tall",
+                    messenger_extensions: true
+                },
+                {
+                    type: 'postback',
+                    title: 'Kết nối lại',
+                    payload: 'STUDENT_CONNECT',
+                },
+            ]);
+            return;
+        }
+        client.sendText(psid, 'Kết nối tài khoản thành công!');
+        data.message && client.sendText(psid, data.message);
+        const account = await Account.findOne({ ps_id: psid, studentCode: student_code });
+        if (!account) return await Account.create({ ps_id: psid, studentCode: student_code, password: student_pass });
+        account.password = student_pass;
+        await account.save();
+    } catch (error) {
+        client.sendText(psid, 'Có lỗi xảy ra trong quá trình kết nối');
+        client.sendText(psid, error.message);
     }
-    client.sendText(psid, 'Kết nối tài khoản thành công!');
-    data.message && client.sendText(psid, data.message);
-    const account = await Account.findOne({ ps_id: psid, studentCode: student_code });
-    if (!account) return await Account.create({ ps_id: psid, studentCode: student_code, password: student_pass });
-    account.password = student_pass;
-    await account.save();
+
 }
--- handlers/events/cooking/index.js
+++ handlers/events/cooking/index.js
@@ -2,10 +2,14 @@ const Dish = require("../../../models/Dish");
 const elementDish = require("./element");
 module.exports = function (client, psid) {
     const elements = [];
-    Dish.findRandom(5).then(function (dishes) {
-        dishes.forEach(function (dish) {
-            elements.push(elementDish(dish));
-        });
-        client.sendGenericTemplate(psid, elements, { image_aspect_ratio: 'square' });
-    });
+    Dish.findRandom(5)
+        .then(function (dishes) {
+            dishes.forEach(function (dish) {
+                elements.push(elementDish(dish));
+            });
+            client.sendGenericTemplate(psid, elements, { image_aspect_ratio: 'square' });
+        })
+        .catch(function (error) {
+            client.sendText(psid, error.message);
+        })
 }
